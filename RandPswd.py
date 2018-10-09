15e4b60886485e7d0fa97f8467973d6582dd1f15
Chloe <26838744+lyricalpaws@users.noreply.github.com>
2018-10-09 09:27:16 +0100

    Update RandPswd.py

--- RandPswd.py
+++ RandPswd.py
@@ -1,15 +1,41 @@
+# Importing modules for the program.
 import random
+import sys
 
-chars = 'abcdefghijklmnopqrstuvwxyz1234567890'
+# Setting the characters that the generator can use when making passwords.
+chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-!?$'
 
-length = input("Password length? ")
-length = int(length)
+# Asks user how long each password should be, and requires an input.
+length = input("Password length?\n> ")
 
-amount = input("How many? ")
-amount = int(amount)
+# Checks if the inputted value is a number or not.
+# If it is, set it as an integer. If not, return a message and end the process.
+if length.isnumeric():
+    length = int(length)
+else:
+    sys.exit("That isn't a number!")
 
+# Same as before, but for number of passwords rather than length.
+amount = input("How many passwords?\n> ")
+
+# Also same as before with checks.
+if amount.isnumeric():
+    amount = int(amount)
+else:
+    sys.exit("That isn't a number!")
+
+
+# If the user only wants one password it will say "Your password is",
+# but if they wanted multiple passwords then it will say "Your passwords are:".
+if amount > 1:
+    print("Your passwords are:")
+else:
+    print("Your password is:")
+
+# Generates a password per the number of times said in the amount variable,
+# then prints each password individually as a random string from the chars variable.
 for p in range(amount):
     password = ''
     for c in range(length):
         password += random.choice(chars)
-    print("Your password is: " + password)
+    print(password)
